6019f73fa3185f86b3cc260a07d9dad0f34ad497
Luz <luli.vercesi@gmail.com>
2024-04-02 17:06:38 -0400

    add some plots to Explore tab

--- app.R
+++ app.R
@@ -2,24 +2,48 @@ library(plotly)
 library(DT)
 
 ui <- navbarPage("Data", id="nav",
+  # File ettings and filters
 	tabPanel("Settings",
 		sidebarLayout(
 			sidebarPanel(
 				fileInput("upload", NULL,
-				          accept=c(".txt",".text",".csv",".tsv",".xls",".xlsx")),
+				          accept=c(".txt",".text",".csv",".tsv",".xls",".xlsx"),
+				          placeholder="Select file to begin"),
 				tableOutput("filesummary")
 			),
-			mainPanel("Main contents",
-			          checkboxInput("headerbool", "Header", value = TRUE),
-			          #checkboxInput("transpose", "Transpose", value = FALSE),
-			          selectInput("cols", "Keep columns", choices=NULL, multiple=TRUE),
-				tableOutput("table")
+			mainPanel(
+			  fluidRow(
+			    column(6, checkboxInput("headerbool", "Set first row as header", value = TRUE)),
+			    #checkboxInput("transpose", "Transpose", value = FALSE)
+			    column(6, selectInput("cols", "Keep columns", choices=NULL, multiple=TRUE))
+			  ),
+			  tableOutput("preview")
 			)
 		)
 	),
 	
+  # Data exploration
 	tabPanel("Explorer",
-	         dataTableOutput("filtered")
+	   fluidRow(
+	     column(8, DT::dataTableOutput("filtered")),
+	     column(4,
+	       fluidRow(
+	         selectInput("plotType", "Plot Type",
+	                     c(Scatter = "scatter", Histogram = "hist")
+	         ),
+	         conditionalPanel(
+	           condition = "input.plotType == 'scatter'",
+	           selectizeInput("scattervars", "Scatter variables", choices=NULL, options=list(maxItems=2)),
+	           selectizeInput("scattercol", "Color by", choices="None", options=list(maxItems=1))
+	         ),
+	         conditionalPanel(
+	           condition = "input.plotType == 'hist'",
+	           selectizeInput("histvar", "Histogram variable", choices=NULL, options=list(maxItems=1))
+	         )
+	       ),
+	       fluidRow(plotlyOutput("plot"))
+	     )
+	   )
 	)
 )
 
@@ -27,8 +51,31 @@ server <- function(input, output, session) {
   observe({ # Update column names input
     req(input$upload)
     updateSelectInput(session, "cols",
-                      choices = colnames(data()),
-                      selected = colnames(data()))
+                      choices = names(data()),
+                      selected = names(data()))
+  })
+  
+  observe({ # Update scatter variables
+    req(filteredData())
+    
+    varOptions <- names(which(sapply(filteredData(), is.numeric)))
+    colOptions <- names(which(sapply(filteredData(), is.character)))
+    
+    if(length(varOptions)>0){
+      updateSelectizeInput(session, "scattervars",
+                      choices = varOptions,
+                      selected = varOptions[c(1,2)])
+      
+      updateSelectizeInput(session, "histvar",
+                           choices = varOptions,
+                           selected = varOptions[1])
+    }
+    
+    if(length(colOptions)>0){
+      updateSelectizeInput(session, "scattercol",
+                           choices = c("None", colOptions),
+                           selected = "None")
+    }
   })
   
   filesummary <- reactive({
@@ -66,14 +113,41 @@ server <- function(input, output, session) {
   
   # Filtered dataset
   filteredData <- reactive({
-    if(any(!input$cols %in% colnames(data())) | is.null(input$cols)) data() else data()[,input$cols]
+    if(any(!input$cols %in% colnames(data())) | is.null(input$cols)){
+      data()
+    }else{
+      data()[,input$cols]
+    }
   })
   
-	output$filesummary <- renderTable(filesummary(), width="100%")
+  # Outputs
+	output$filesummary <- renderTable(filesummary(), width="100%",
+	                                  caption="File summary", caption.placement="top")
 	
-	output$table <- renderTable(head(filteredData()))
+	output$preview <- renderTable(head(filteredData()), width="100%",
+	                              caption="Dataset preview", caption.placement="top")
 	
-	output$filtered <- renderDataTable(filteredData())
+	output$filtered <- DT::renderDataTable(filteredData(), options = list(scrollX = TRUE))
+	
+	output$plot <- renderPlotly({
+	  req(filteredData())
+	  
+	  # Scatter plot
+	  if (input$plotType == "scatter") {
+	    req(length(input$scattervars)==2)
+	    plot_ly(data = filteredData(), 
+	          x = ~get(input$scattervars[1]), 
+	          y = ~get(input$scattervars[2]), 
+	          mode = "markers",
+	          type = "scatter",
+	          color = if(input$scattercol=="None") NULL else ~get(input$scattercol)) %>% 
+	       layout(xaxis = list(title = as.character(input$scattervars[1])),
+	              yaxis = list(title = as.character(input$scattervars[2])))
+	  } else if (input$plotType == "hist"){
+	    plot_ly(x = ~filteredData()[input$histvar], 
+	            type = "histogram")
+	  }
+	})
 }
 
 shinyApp(ui, server)
